74dc5f07c53f3c3af328c0841562b724508d9d52
bibekg <bibekgurung@inficare.com.my>
2020-09-02 22:30:24 +0545

    End Of Section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,7 +5,6 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
-
 import { AppComponent } from './app.component';
 import {NavComponent} from './nav/nav.component';
 import {AuthService} from './_services/auth.service';
@@ -13,6 +12,12 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider} from './_services/error.interceptor';
 import { AlertifyService} from './_services/alertify.service';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import {appRoutes} from './routes';
+import {AuthGuard} from './_guards/auth.guard';
 
 
 @NgModule({
@@ -20,19 +25,24 @@ import { AlertifyService} from './_services/alertify.service';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
     AlertifyService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
